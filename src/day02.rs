770c82e9067acf154a5ff058d04c870d8ba79350
GP2015 <adamcunningham2015@hotmail.com>
2025-11-29 21:46:53 +0000

    Finished Day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,16 +1,28 @@
 use crate::Part;
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 
 pub fn run_day_2(part: Part, input: &String) -> Result<()> {
     match part {
-        Part::A => general(get_keypad(KeypadType::A), input)?,
-        Part::B => general(get_keypad(KeypadType::B), input)?,
+        Part::A => part_a(input)?,
+        Part::B => part_b(input)?,
         Part::Both => {
-            general(get_keypad(KeypadType::A), input)?;
-            general(get_keypad(KeypadType::B), input)?;
+            part_a(input)?;
+            part_b(input)?;
         }
     }
 
+    Ok(())
+}
+
+fn part_a(input: &String) -> Result<()> {
+    print!("Day 2 Part A: ");
+    general(Keypad::type_a(), input)?;
+    Ok(())
+}
+
+fn part_b(input: &String) -> Result<()> {
+    print!("Day 2 Part B: ");
+    general(Keypad::type_b(), input)?;
     Ok(())
 }
 
@@ -21,11 +33,11 @@ struct Position {
 
 impl Position {
     fn new(x: isize, y: isize) -> Self {
-        Position { x, y }
+        Self { x, y }
     }
 
     fn get_shifted(&self, dx: isize, dy: isize) -> Self {
-        Position {
+        Self {
             x: self.x + dx,
             y: self.y + dy,
         }
@@ -41,25 +53,48 @@ enum Direction {
 
 const NOL: &str = "";
 
+macro_rules! string_vec {
+    ( $( $x:expr ),* ) => {
+        vec![ $( String::from($x) ),* ]
+    };
+}
+
 struct Keypad {
     labels: Vec<Vec<String>>,
-    selected: Position,
+    position: Position,
 }
 
 impl Keypad {
-    fn new(labels: Vec<Vec<String>>, start_position: Position) -> Self {
-        Keypad {
-            labels,
-            selected: start_position,
+    fn type_a() -> Self {
+        Self {
+            labels: vec![
+                string_vec!("1", "2", "3"),
+                string_vec!("4", "5", "6"),
+                string_vec!("7", "8", "9"),
+            ],
+            position: Position::new(1, 1),
+        }
+    }
+
+    fn type_b() -> Self {
+        Self {
+            labels: vec![
+                string_vec!(NOL, NOL, "1", NOL, NOL),
+                string_vec!(NOL, "2", "3", "4", NOL),
+                string_vec!("5", "6", "7", "8", "9"),
+                string_vec!(NOL, "A", "B", "C", NOL),
+                string_vec!(NOL, NOL, "D", NOL, NOL),
+            ],
+            position: Position::new(2, 2),
         }
     }
 
     fn shift(&mut self, direction: Direction) {
         let new_pos = match direction {
-            Direction::Left => self.selected.get_shifted(-1, 0),
-            Direction::Right => self.selected.get_shifted(1, 0),
-            Direction::Up => self.selected.get_shifted(0, -1),
-            Direction::Down => self.selected.get_shifted(0, 1),
+            Direction::Left => self.position.get_shifted(-1, 0),
+            Direction::Right => self.position.get_shifted(1, 0),
+            Direction::Up => self.position.get_shifted(0, -1),
+            Direction::Down => self.position.get_shifted(0, 1),
         };
 
         if new_pos.y < 0 || new_pos.y >= self.labels.len() as isize {
@@ -78,52 +113,31 @@ impl Keypad {
             return;
         }
 
-        self.selected = new_pos;
+        self.position = new_pos;
+    }
+
+    fn get_label(&self) -> String {
+        self.labels[self.position.y as usize][self.position.x as usize].clone()
     }
 }
 
-enum KeypadType {
-    A,
-    B,
-}
+fn general(mut keypad: Keypad, input: &String) -> Result<()> {
+    let mut code = String::new();
 
-macro_rules! string_vec {
-    ( $( $x:expr ),* ) => {
-        vec![ $( String::from($x) ),* ]
-    };
-}
+    for instruction in input.lines() {
+        for c in instruction.chars() {
+            match c {
+                'L' => keypad.shift(Direction::Left),
+                'R' => keypad.shift(Direction::Right),
+                'U' => keypad.shift(Direction::Up),
+                'D' => keypad.shift(Direction::Down),
+                _ => return Err(anyhow!("Invalid input: {}.", c)),
+            }
+        }
 
-fn get_keypad(keypad_type: KeypadType) -> Keypad {
-    let labels = match keypad_type {
-        KeypadType::A => vec![
-            string_vec!("1", "2", "3"),
-            string_vec!("4", "5", "6"),
-            string_vec!("7", "8", "9"),
-        ],
-        KeypadType::B => vec![
-            string_vec!(NOL, NOL, "1", NOL, NOL),
-            string_vec!(NOL, "2", "3", "4", NOL),
-            string_vec!("5", "6", "7", "8", "9"),
-            string_vec!(NOL, "A", "B", "C", NOL),
-            string_vec!(NOL, NOL, "D", NOL, NOL),
-        ],
-    };
-
-    let position = match keypad_type {
-        KeypadType::A => Position::new(1, 1),
-        KeypadType::B => Position::new(2, 2),
-    };
-
-    Keypad::new(labels, position)
-}
-
-fn general(keypad: Keypad, input: &String) -> Result<()> {
-    let instructions = input.trim().split("\n");
-
-    for instruction in instructions {
-        for char_index in 0..instruction.len() {}
+        code += keypad.get_label().as_str();
     }
 
-    // println!("Answer: {}", pos[0].abs() + pos[1].abs());
+    println!("{}", code);
     Ok(())
 }
