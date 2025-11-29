a8e36cb9d1b02841d015835686a73477e2b4ff8a
GP2015 <adamcunningham2015@hotmail.com>
2025-11-29 20:24:27 +0000

    Refactored Day 1

---
+++ src/day1.rs
@@ -0,0 +1,75 @@
+use crate::Part;
+use anyhow::Result;
+
+pub fn run_day_1(part: Part, input: &String) -> Result<()> {
+    match part {
+        Part::A => part_a(input)?,
+        Part::B => part_b(input)?,
+        Part::Both => {
+            part_a(input)?;
+            part_b(input)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn part_a(input: &String) -> Result<()> {
+    let instructions = input.trim().split(", ");
+
+    let mut pos = [0, 0];
+    let mut dir = [1, 0];
+
+    for instruction in instructions {
+        let (turn, steps_str) = instruction.split_at(1);
+        let steps: i32 = steps_str.parse()?;
+
+        dir = match turn {
+            "R" => [dir[1], -dir[0]],
+            "L" => [-dir[1], dir[0]],
+            _ => panic!("Invalid input."),
+        };
+
+        for i in 0..2 {
+            pos[i] += dir[i] * steps;
+        }
+    }
+
+    println!("Day 1 Part A: {}", pos[0].abs() + pos[1].abs());
+    Ok(())
+}
+
+fn part_b(input: &String) -> Result<()> {
+    let instructions = input.trim().split(", ");
+
+    let mut pos: [i32; 2] = [0, 0];
+    let mut dir = [1, 0];
+
+    let mut past_locations = vec![[0, 0]];
+
+    'main_loop: for instruction in instructions {
+        let (turn, steps_str) = instruction.split_at(1);
+        let steps: i32 = steps_str.parse()?;
+
+        dir = match turn {
+            "R" => [dir[1], -dir[0]],
+            "L" => [-dir[1], dir[0]],
+            _ => panic!("Invalid input."),
+        };
+
+        for _ in 0..steps {
+            for i in 0..2 {
+                pos[i] += dir[i];
+            }
+
+            if past_locations.contains(&pos) {
+                break 'main_loop;
+            }
+
+            past_locations.push(pos);
+        }
+    }
+
+    println!("Day 1 Part B: {}", pos[0].abs() + pos[1].abs());
+    Ok(())
+}
